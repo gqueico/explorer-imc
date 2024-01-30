634d280931660b031672253f22924e712f3b049a
gqueico <gqueico@gmail.com>
2024-01-30 17:31:21 -0300

    :hammer: Modularizing the error alert box

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
 
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
@@ -10,11 +11,24 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if(showAlertError) {
+    AlertError.open()
+    return;
+  } 
+  
+  AlertError.close()
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
   Modal.open()
+}
+
+const notANumber = (value) => {
+  return isNaN(value) || value == ""
 }
 
 const IMC = (weight, height) => (weight / ((height / 100) ** 2)).toFixed(2)

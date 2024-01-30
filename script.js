c8fcc1e97b5ca088bba3bc623e6f3184516fbc0e
gqueico <gqueico@gmail.com>
2024-01-29 23:16:42 -0300

    :hammer: Structuring Modal data

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
-const modalWrapper = document.querySelector(".modal-wrapper")
-const modalMessage = document.querySelector(".modal-wrapper .title")
-const modalBtnClose = document.querySelector(".modal-wrapper .close")
-
-const IMC = (weight, height) => {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
+const Modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title"),
+  buttonClose: document.querySelector(".modal .close"),
+  open() {
+    Modal.wrapper.classList.add("open")
+  },
+  close() {
+    Modal.wrapper.classList.remove("open")
+  }
 }
 
-form.onsubmit = (event) => {
+form.onsubmit = event => {
   event.preventDefault()
 
   const weight = inputWeight.value
@@ -19,8 +23,12 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add("open")
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove("open")
+Modal.buttonClose.onclick = () => {
+  Modal.close()
+}
+
+const IMC = (weight, height) => (weight / ((height / 100) ** 2)).toFixed(2)
